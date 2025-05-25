f8f91f10faac02ad5c60abdcdd44a6f951054e2d
its-ani <guptaanirudh712@gmail.com>
2025-05-25 16:26:44 +0530

    added order service

---
+++ services/order/src/main/java/dev/anirudh/ecommerce/orderline/OrderLine.java
@@ -0,0 +1,22 @@
+package dev.anirudh.ecommerce.orderline;
+
+import dev.anirudh.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private Integer productId;
+}
