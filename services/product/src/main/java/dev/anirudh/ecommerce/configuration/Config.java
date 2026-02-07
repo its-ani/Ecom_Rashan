4bdad59c4fd73bcae7669aeb719bb4ee0709b89c
its-ani <guptaanirudh712@gmail.com>
2026-02-07 16:45:37 +0530

    Added elasticSearch config file in Product service.

---
+++ services/product/src/main/java/dev/anirudh/ecommerce/configuration/Config.java
@@ -0,0 +1,28 @@
+package dev.anirudh.ecommerce.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "dev.anirudh.ecommerce.repository")
+@ComponentScan(basePackages = {"dev.anirudh.ecommerce"})
+public class Config extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    public String elasticsearchUrl;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        String hostAndPort = elasticsearchUrl.replace("http://", "").replace("https://", "");
+
+        return ClientConfiguration.builder()
+                .connectedTo(hostAndPort)
+                .withConnectTimeout(10000)
+                .withSocketTimeout(30000)
+                .build();
+    }
+}
